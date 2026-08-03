516104d42fd0e8a1c738bb0509f9a1b3e2936fac
laveesh21 <laveeshtomar.royalking@gmail.com>
2026-08-03 23:45:02 +0530

    fix: book routes fix and cloudinary configuration

--- server/src/routes/bookRoute.js
+++ server/src/routes/bookRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/auth.middleware.js";
 import Book from "../models/Book.js";
+import cloudinary from "../utils/cloudinary.js";
 const router = express.Router();
 
+// GET ALL BOOKS
 router.get("/", async (req, res) => {
   try {
-    const { limit, page } = req.query;
+    let { limit, page } = req.query;
     limit = parseInt(limit) || 10;
     page = parseInt(page) || 1;
     const offset = (page - 1) * limit;
@@ -24,6 +26,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET USER'S BOOKS
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const books = await Book.find({ user: userId }).sort({ createdAt: -1 });
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// FIND BOOK BY ID
 router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -41,36 +55,40 @@ router.get("/:id", async (req, res) => {
 });
 
 // CREATE BOOK
-router.post("/addbook", protectRoute, async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
+
     const { title, description, rating, coverImage } = req.body;
 
-    if (!title || !description || !rating) {
-      return res.status(400).json({ message: "All fields are required" });
+    if (!coverImage || !title || !description || !rating) {
+      return res.status(400).json({ message: "Please provide all fields" });
     }
 
-    if (coverImage) {
-      const uploadResponse = await cloudinary.uploader.upload(coverImage, {
-        folder: "books",
-      });
-      coverImage = uploadResponse.secure_url;
-    }
+    // upload the image to cloudinary
+    const result = await cloudinary.uploader.upload(req.body.coverImage, {
+      folder: "book-covers",
+    });    
+    const imageUrl = result.secure_url;
 
+    // save to the database
     const newBook = new Book({
       title,
       description,
       rating,
-      coverImage: " coverImage",
+      coverImage: imageUrl,
       user: req.user.userId,
     });
 
-    console.log("New book instance created:", newBook);
+    console.log("BOOK===>", newBook)
+
     await newBook.save();
-    console.log("New book saved to database:", newBook);
+
+    console.log("BOOK SAVED ----------------------------------")
 
     res.status(201).json(newBook);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Error creating book", error);
+    res.status(500).json({ message: error.message });
   }
 });
 
