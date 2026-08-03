0088e15313fa5379630733046aeec76a9c9f0018
laveesh21 <laveeshtomar.royalking@gmail.com>
2026-08-03 17:23:59 +0530

    cors update

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,18 @@ import bookRoute from "./routes/bookRoute.js"
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8081",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
+
+
 
 app.use("/api/auth", authRoute)
 app.use("/api/books", bookRoute)
