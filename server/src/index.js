3a902543d4f88935f536dcad87b8325971c08dd5
laveesh21 <laveeshtomar.royalking@gmail.com>
2026-08-13 13:10:08 +0530

    payload accepting size increased

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ import bookRoute from "./routes/bookRoute.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
